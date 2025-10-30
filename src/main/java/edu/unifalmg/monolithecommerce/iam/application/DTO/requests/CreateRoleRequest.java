e64a9af366070fd78bd7b2a954b8796245cf0ac4
MaiconMian <maicon.mian@sou.unifal-mg.edu.br>
2025-10-30 14:38:33 -0300

    feat: adding role application layer

---
+++ src/main/java/edu/unifalmg/monolithecommerce/iam/application/DTO/requests/CreateRoleRequest.java
@@ -0,0 +1,4 @@
+package edu.unifalmg.monolithecommerce.iam.application.DTO.requests;
+
+public record CreateRoleRequest (String name, String description) {
+}
---
+++ src/main/java/edu/unifalmg/monolithecommerce/iam/application/userCase/GetRoleByIdUserCase.java
@@ -0,0 +1,25 @@
+package edu.unifalmg.monolithecommerce.iam.application.userCase;
+
+import edu.unifalmg.monolithecommerce.iam.application.DTO.RoleDTO;
+import edu.unifalmg.monolithecommerce.iam.application.DTO.commands.GetRoleByIdCommand;
+import edu.unifalmg.monolithecommerce.iam.application.mapper.RoleMapper;
+import edu.unifalmg.monolithecommerce.iam.application.port.in.GetRoleByIdPort;
+import edu.unifalmg.monolithecommerce.iam.application.port.out.RoleRepositoryPort;
+import edu.unifalmg.monolithecommerce.iam.domain.model.Role;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GetRoleByIdUserCase implements GetRoleByIdPort {
+    private final RoleRepositoryPort roleRepository;
+    private final RoleMapper roleMapper;
+
+    @Override
+    @Transactional
+    public RoleDTO execute (GetRoleByIdCommand cmd){
+        Role roleFound = roleRepository.findById(cmd.id());
+        return roleMapper.toDTO(roleFound);
+    }
+}
