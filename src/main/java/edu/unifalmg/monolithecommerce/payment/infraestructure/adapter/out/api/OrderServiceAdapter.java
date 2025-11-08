d00499188de3c7496de16907c1f14ba6fc63f9b2
MaiconMian <maicon.mian@sou.unifal-mg.edu.br>
2025-11-07 21:04:44 -0300

    feat: adding a order service adapter

---
+++ src/main/java/edu/unifalmg/monolithecommerce/payment/infraestructure/adapter/out/api/OrderServiceAdapter.java
@@ -0,0 +1,31 @@
+package edu.unifalmg.monolithecommerce.payment.infraestructure.adapter.out.api;
+
+import edu.unifalmg.monolithecommerce.order.infratestructure.adapter.out.api.*;
+import edu.unifalmg.monolithecommerce.payment.application.port.out.OrderServicePort;
+import edu.unifalmg.monolithecommerce.shared.domain.model.Money;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class OrderServiceAdapter implements OrderServicePort {
+
+    private final GetItemsByOrderIdPort getItemsByOrderIdPort;
+    private final GetPayerByOrderIdPort getPayerByOrderIdPort;
+    private final GetTotalAmountOrderByOrderIdPort getTotalAmountOrderByOrderIdPort;
+
+    @Override
+    public List<Item> getItemListByOrderId(OrderId orderid){
+        return getItemsByOrderIdPort.execute(orderid);
+    };
+
+    @Override
+    public Payer getPayerByOrderId(OrderId orderid){
+        return getPayerByOrderIdPort.execute(orderid);
+    };
+
+    @Override
+    public Money getTotalAmountOrderByOrderId(OrderId orderid) { return getTotalAmountOrderByOrderIdPort.execute(orderid);}
+}
